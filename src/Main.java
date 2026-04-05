4264ccef2d1c1f5d9e4e0b8f64fb6a6181eac4f4
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-04-04 21:39:36 -0300

    Ex01:Ímpar ou par

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
 
+ Integer num1;
+
+
+        System.out.println("Digite um número");
+        num1 = sc.nextInt();
+
+        if(num1 % 2 == 0){
+            System.out.println("Este numero e par");
+
+        }
+        else{
+            System.out.println("Este numero e ímpar");
+        }
     }
 }
