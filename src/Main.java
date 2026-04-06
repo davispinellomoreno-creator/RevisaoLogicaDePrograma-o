925f421283ff5e7726b9e4db91269b48d63afd35
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-04-05 21:42:50 -0300

    Segundo exercício feito

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,29 @@ public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
- Integer num1;
+      Integer num1;
+        Integer num2;
+        Integer num3;
 
-
-        System.out.println("Digite um número");
+//Maior de três números:Leia três números e exiba qual é o maior deles.
+        System.out.println("Digite um número:");
         num1 = sc.nextInt();
-
-        if(num1 % 2 == 0){
-            System.out.println("Este numero e par");
+        System.out.println("Digite um número:");
+        num2 = sc.nextInt();
+        System.out.println("Digite um número:");
+        num3 = sc.nextInt();
+        if(num1>num2 && num1>num3){
+            System.out.println("Este numero" + num1 +  " é o maior");
 
         }
+        else if(num2>num1 && num2>num3){
+            System.out.println("Este numero " + num2 + " é o maior");
+        }
+        else if(num3>num1 && num3>num2){
+            System.out.println("Este numero " + num3 + " é o maior");
+        }
         else{
-            System.out.println("Este numero e ímpar");
+            System.out.println("ERRO!");
         }
     }
 }
