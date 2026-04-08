ba6862f1540baadf90bb446f7a745d9fcce2bbe6
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-04-07 22:39:26 -0300

    Pequenas medanças para finalizar esta atividade amnhã

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,22 @@ public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
-      Integer num1;
+      int num1;
+      int soma = 0;
 
 
 //Leia um número N e calcule a soma de todos os números pares de 1 até N.
         System.out.println("Digite um número:");
         num1 = sc.nextInt();
 
-        if(){
 
-        }
+        for (int i = 0; i <= num1; i++){
+     if(i % 2 == 0){
+         soma += i;
+
+         System.out.println("soma dos pares :" + soma);
+     }
+ }
 
     }
 }
