d27529fff9c216db0950fbf2447cb4a2740db853
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-04-11 22:07:52 -0300

    Falta so testar o exercicio, mas está praticamente xconcluido, estav sem tempo para fazer de uma vez só

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,31 @@ public class Main {
       Scanner sc = new Scanner(System.in);
 
       int num1;
-      int soma = 0;
-
+     boolean primo = true;
 
 //Verifique se um número informado é primo ou não.
         System.out.println("Digite um número:");
         num1 = sc.nextInt();
 
-        if(num1/1 =){
-            System.out.println("Este número é primo");
+        if(num1 < 2) {
+            primo = false;
+        }
+        else {
+            for (int i = 2; i <= Math.sqrt(num1); i++) {
+                if (num1 % i == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+        }
+        if (primo) {
+            System.out.println("É primo");
+        } else {
+            System.out.println("Não é primo");
         }
 
-
     }
+
 }
 
 
