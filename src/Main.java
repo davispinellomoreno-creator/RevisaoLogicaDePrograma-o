9827b032ce68420f6ad6ccf18e2e443b5a5d0ad1
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-04-16 22:42:49 -0300

    Començando a fazer o penúltimo exercicio

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
-        // Inverter número
-        //
-        //Peça um número inteiro (ex: 1234) e mostre ele invertido (4321).
-
-        int num1=1234;
-        int invertido = 0;
-
-        while(num1 != 0){
-            int digito = num1 % 10;
-            invertido = invertido * 10 + digito;
-            num1 = num1/10;
-
-        }
-        System.out.println("Numero invertido :" +
-                 + invertido);
+// Sequência de Fibonacci
+//
+//Mostre os primeiros N números da sequência de Fibonacci.
+//Ex: 0, 1, 1, 2, 3, 5...
 
     }
 }
