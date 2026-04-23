d9076496cdfd677c82ff7406be1d298668c83320
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-04-23 09:07:27 -0300

    Salvando esta versão,para não se perder

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Integer senha;
+        Integer senha = 2202;
+        Integer entrada;
 
-        System.out.println("Digite a senha!");
+        System.out.println("Digite a senha! : ");
         senha = sc.nextInt();
         //Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
         //incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
         //impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
 
-        while ( senha == 2202) {
-            if(senha == 2202) {
+        do {
+            System.out.println("Digite a senha:");
+            entrada = sc.nextInt();
+
+            if (!entrada.equals(senha)) {
                 System.out.println("Senha correta!");
+
             }
-        else{
-                System.out.println("Senha ivalida!");
-            }
+
+
+        }
+while(!entrada.equals(senha));{
+            System.out.println("Incorreta");
+
+        }
         }
     }
-}
+
 
 
 
