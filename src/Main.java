c4322607c79c9873e91b323b06799b39e67d1d66
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-04-24 16:24:58 -0300

    Iniciando mais um exercicío do curso do basico ao avançado do professor Nélio Alves

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Integer entrada;
 
 
-        //Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
-        //incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
-        //impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
 
-    while(true){
-        System.out.println("Digite A senha: ");
-        entrada = sc.nextInt();
+        //Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+        //cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+        //menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
 
-        if(entrada == 2000){
-            System.out.println("Senha correta");
-            break;
-
-        }
-        else{
-            System.out.println("Valor invalido tente novamente");
-
-        }
-    }
-    sc.close();
         }
     }
 
