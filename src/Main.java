2ea633f59daea33f13d9dbabae4385b8110809e2
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-04-25 22:59:08 -0300

    Finalizando projeto do curso Nélio Alves, pelo tempo só deu pra declarear as variaveis

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+         Integer coordenadaX;
+         Integer coordenadaY;
 
+        System.out.println("Digite a primeira coordenada:");
+        coordenadaX = sc.nextInt();
 
-
+        System.out.println("Digite a primeira coordenada:");
+        coordenadaY = sc.nextInt();
 
         //Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
         //cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
